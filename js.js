16eee72d512ab39287c6482cd7e6a6690633029b
premiacaojaneiro <wolfhousebr1@gmail.com>
2026-01-08 00:24:03 -0300

    Update js.js

--- js.js
+++ js.js
@@ -201,7 +201,7 @@ function showUrgentAlert() {
     // 3. ADICIONA O EVENTO (AGORA O BOTÃO EXISTE, NÃO VAI DAR NULL)
     if (btnModal) {
         btnModal.onclick = () => {
-            window.location.href = 'https://pay.sunize.com.br/ylxxHfvm';
+            window.location.href = 'https://pay.risepay.com.br/Pay/f81c8181b2fb4d4d93b1112dc31d65d8';
         };
     }
 
@@ -209,4 +209,5 @@ function showUrgentAlert() {
     overlay.onclick = (e) => {
         if (e.target === overlay) overlay.remove();
     };
+
 }
